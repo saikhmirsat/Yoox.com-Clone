b3d8662baf47b46ac83d3a0754dcbdbe129d3051
MD Sakil Ahmed <sakilahmed965@gmail.com>
2022-12-17 18:37:17 +0530

    dashboard ui updated

--- src/admin/AdminIndex.jsx
+++ src/admin/AdminIndex.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./index.css"
-
+import Dashboard from "./dashboard";
+import "./index.css";
+import ProductDetails from "./product_form";
 
 const AdminIndex = () => {
-    return (
-        <div className="admin_heading">
-            <Link to={"/add_products"}>ADD PRODUCTS</Link>
-            <Link to={"/dashboard"}>DASHBOARD</Link>
+  return (
+    <div>
+      {/* <div className="admin_heading">
+        <Link to={"/add_products"}>ADD PRODUCTS</Link>
+        <Link to={"/dashboard"}>DASHBOARD</Link>
+      </div> */}
+      <div className="admin_panel">
+            <div className="form"><ProductDetails/></div>
+            <div className="dash"><Dashboard/></div>
         </div>
-    );
+    </div>
+  );
 };
 
 export default AdminIndex;
