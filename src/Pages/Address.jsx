9c9345d6ec109bf9bb4b8a8a06e097da08c7fe9c
saikhmirsat <saikh.mirsat.786@gmail.com>
2022-12-18 19:24:26 +0530

    address page

--- src/Pages/Address.jsx
+++ src/Pages/Address.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-import './Address.css'
+import "./Address.css"
 
-export default function Address() {
+const Address = () => {
     return (
-        <div>Address</div>
+        <div style={{ alignItems: "center", margin: "auto" }}>
+            <div style={{ fontSize: "20px" }}>Address</div>
+            <div className='address'>
+                <form action="">
+                    <label htmlFor="">
+                        Name : <input type="text" placeholder='Name' required />
+                    </label>
+                    <label htmlFor="">
+                        Mobile No : <input type="number" placeholder='Mobile' required />
+                    </label>
+                    <label htmlFor="">
+                        Address : <textarea name="" id="" cols="30" rows="5" placeholder='Enter your address ...' required></textarea>
+                    </label>
+                    <label htmlFor="">
+                        Payment Through Cash on Delevery : <input type="checkbox" required />
+                    </label>
+
+                    <input type="submit" value="Place Order" onClick={() => alert("Congratulations! Odrer Successfull !")} />
+                </form>
+            </div>
+        </div>
+
     )
 }
+
+export default Address
