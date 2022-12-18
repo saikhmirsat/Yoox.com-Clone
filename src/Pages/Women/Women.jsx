80f3efb68e2402655119919c2cf0001a3e10d0ae
saikhmirsat <saikh.mirsat.786@gmail.com>
2022-12-18 19:13:10 +0530

    women cart part

--- src/Pages/Women/Women.jsx
+++ src/Pages/Women/Women.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
 import WomenNavbar from './WomenNavbar'
 import "./women.css"
-import {WomenCarousel,OneInAllCarousel} from './WomenCarousel'
+import { WomenCarousel, OneInAllCarousel } from './WomenCarousel'
 import { CarouselCard } from './CarouselCard'
 import { HiArrowLeft, HiArrowRight } from 'react-icons/hi2'
 import ImagePlusSlider from './Image-plus-slider'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Women() {
 
-  
+  const navigate = useNavigate()
 
-  
+  const viewallfunc = () => {
+    navigate('/womenItem')
+  }
+
+
   return (
-    <div style={{backgroundColor:"#f3f3f3"}} >
+    <div style={{ backgroundColor: "#f3f3f3" }} >
       <WomenNavbar />
 
 
-       <div className='first-container' >
-        <img src="https://www.yoox.com/images/yoox80/banners/6895_1_Genz_WM_Main.png" width="1340px" height="640px"  alt="" />
+      <div className='first-container' >
+        <img src="https://www.yoox.com/images/yoox80/banners/6895_1_Genz_WM_Main.png" width="1340px" height="640px" alt="" />
         <div className='here-comes-night' >
           <h2>HERE COMES THE NIGHT</h2>
           <p>Rock-inspired leather looks and daring details</p>
           <div>
             <span>GET THE LOOK</span>
             <span className='discover' >DISCOVER MORE</span>
-            
+
           </div>
           <br />
         </div>
@@ -34,15 +40,15 @@ export default function Women() {
         <div className="designer-item">
           <img src="https://www.yoox.com/images/yoox80/banners/6824_3_WinterChecklist_HL_W.jpg?634485886601286852#width=430&height=600" alt="" />
           <div className='designer-item-div' >
-          <h2>IT'S COLD OUTSIDE</h2>
-          <p>Everything you need to stay warm this winter</p>
-          <div>
-            <span>SHOP NOW </span>
-            
-            
+            <h2>IT'S COLD OUTSIDE</h2>
+            <p>Everything you need to stay warm this winter</p>
+            <div>
+              <span>SHOP NOW </span>
+
+
+            </div>
+            <br />
           </div>
-          <br />
-        </div>
         </div>
         <div className="designers-list">
           <h4>DESIGNERS</h4>
@@ -58,68 +64,68 @@ export default function Women() {
             <p>GUCCI</p>
             <p>PRADA</p>
           </div>
-          <p className='view_all' > View All </p>
+          <p className='view_all' onClick={viewallfunc} > View All </p>
         </div>
         <div className="designer-item">
-         <img src="https://www.yoox.com/images/yoox80/banners/6824_1_XmasGift_W_MainMob.jpg?634485886601286852#width=430&height=600" alt="" />
-         <div className='designer-item-div' >
+          <img src="https://www.yoox.com/images/yoox80/banners/6824_1_XmasGift_W_MainMob.jpg?634485886601286852#width=430&height=600" alt="" />
+          <div className='designer-item-div' >
+            <h2>WOW AT FIRST SIGHT</h2>
+            <p>Special gifts that will leave them speechless</p>
+            <div>
+              <span>SHOP THE MAGIC </span>
+              <span className='discover' >DISCOVER MORE</span>
+
+            </div>
+            <br />
+          </div>
+        </div>
+      </div>
+
+
+      <WomenCarousel />
+
+
+      <div className="brands-container">
+        <div className="brands-container-item">
+          <img src="https://www.yoox.com/images/yoox80/banners/6825_5_BaumeMercier_Tris_W.png  " alt="" />
+          <h2>BAUME & MERCIER</h2>
+          <p>The New Riviera Collection</p>
+        </div>
+        <div id='brands-container-item' className="brands-container-item">
+          <img src="https://www.yoox.com/images/yoox80/banners/6825_1_Boss_W_Tris.jpg" alt="" />
+          <h2>BOSS</h2>
+          <p>Be your own BOSS</p>
+        </div>
+        <div className="brands-container-item">
+          <img src="https://www.yoox.com/images/yoox80/banners/6825_6_Montblanc_W_Tris.jpg" alt="" />
+          <h2>MONTBLANC</h2>
+          <p>The New Gift Generation</p>
+        </div>
+      </div>
+
+
+      <OneInAllCarousel />
+
+      <ImagePlusSlider />
+
+      <div className='last-div'>
+        <img src="https://www.yoox.com/images/yoox80/banners/6824_1_XmasGift_WM_Bottom.jpg" width="1350px" alt="" />
+        <div className='last-div-item' >
           <h2>WOW AT FIRST SIGHT</h2>
           <p>Special gifts that will leave them speechless</p>
           <div>
             <span>SHOP THE MAGIC </span>
-            <span className='discover' >DISCOVER MORE</span>
-            
+            <span className='discover-more' >DISCOVER MORE</span>
+
           </div>
           <br />
         </div>
-        </div>
       </div>
 
 
-       <WomenCarousel />
 
 
-        <div className="brands-container">
-          <div className="brands-container-item">
-            <img src="https://www.yoox.com/images/yoox80/banners/6825_5_BaumeMercier_Tris_W.png  " alt="" />
-            <h2>BAUME & MERCIER</h2>
-            <p>The New Riviera Collection</p>
-          </div>
-          <div id='brands-container-item' className="brands-container-item">
-            <img src="https://www.yoox.com/images/yoox80/banners/6825_1_Boss_W_Tris.jpg" alt="" />
-            <h2>BOSS</h2>
-            <p>Be your own BOSS</p>
-          </div>
-          <div className="brands-container-item">
-            <img src="https://www.yoox.com/images/yoox80/banners/6825_6_Montblanc_W_Tris.jpg" alt="" />
-            <h2>MONTBLANC</h2>
-            <p>The New Gift Generation</p>
-          </div>
-        </div>
-
-
-        <OneInAllCarousel />
-
-        <ImagePlusSlider  />
-
-        <div className='last-div'>
-            <img src="https://www.yoox.com/images/yoox80/banners/6824_1_XmasGift_WM_Bottom.jpg" width="1350px" alt="" />
-            <div className='last-div-item' >
-                <h2>WOW AT FIRST SIGHT</h2>
-                <p>Special gifts that will leave them speechless</p>
-                <div>
-                    <span>SHOP THE MAGIC </span>
-                    <span className='discover-more' >DISCOVER MORE</span>
-                    
-                </div>
-                <br />
-                </div>
-        </div>
-
-        
-      
-        
-      </div>
+    </div>
 
   )
 }
---
+++ src/Pages/Women/WomenDetail.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './WomenDetail.css'
+
+
+export default function WomenDetail() {
+  const [data, setData] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [obj, setObj] = useState({})
+  console.log(obj)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    axios.get(`https://mirsat-vercel-database.vercel.app/yooxData/${id}`)
+      .then((res) => setObj(res.data))
+  }, [])
+
+  const handleAddToCart = (ele) => {
+
+
+    axios.post(`https://product-list-api.onrender.com/cart`, ele)
+      .then((res) => {
+        setCart([...cart, res.data])
+        alert("Product added to the cart !");
+      })
+      .catch((err) => {
+        console.log(err)
+        alert("Product is already added !")
+      })
+
+
+  };
+
+
+  return (
+    <div>
+      <div className='womenDetail-1st-con'>
+        <div>
+
+          <img src={obj.image_1} alt="" />
+
+
+        </div>
+        <div >
+          <h4>{obj.title}</h4>
+          <p>{obj.type}</p>
+          <span style={{ color: 'grey', textDecorationLine: 'line-through' }}>US$388,00</span><span> {obj.offer}%  OFF</span>
+          <h3>US$ {obj.price}</h3>
+          <p>{obj.color}</p>
+          <div style={{ backgroundColor: `${obj.color}`, border: '1px solid black', height: '40px', width: '40px', borderRadius: '50%' }}></div>
+          <span>Size : {obj.size}</span>
+          <button onClick={() => handleAddToCart(obj)}>Add To Bag</button>
+        </div>
+      </div>
+    </div>
+  )
+}
